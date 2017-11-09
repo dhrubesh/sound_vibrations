db6ab6d0cc94729bebdef88c41dff817614a176f
DIO <dhrubesh97@gmail.com>
2017-11-09 19:35:24 +0530

    removed speech recognition

--- index.js
+++ index.js
@@ -73,17 +73,3 @@ var loop = function(){
 };
 
 loop();
-
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
- const recognition = new SpeechRecognition();
- recognition.interimResults = true;
- recognition.addEventListener('result', e => {
-   const transcript = Array.from(e.results)
-     .map(result => result[0])
-     .map(result => result.transcript)
-     .join('');
-     console.log(e.results)
-     console.log(e)
- });
- recognition.addEventListener('end', recognition.start);
- recognition.start();
